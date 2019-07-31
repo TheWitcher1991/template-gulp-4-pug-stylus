4164501e2fab50ef4fb121b707656a9070dcd951
TheWitcher1991 <ashot.svazyan222@gmail.com>
2019-07-31 17:04:15 +0300

    init

---
+++ README.md
@@ -0,0 +1,29 @@
+# Starting template
+Used bundle - `Gulp` + `Webpack` + `Babel` + `Pug` + `Stylus` + `BrowserSync`
+
+## Installation
+
+You will need [Node.js](https://nodejs.org) installed on your computer to download this repository.
+<br>
+or [Yarn](https://yarnpkg.com)
+
+```bash
+$ git clone https://github.com/TheWitcher1991/template-gulp-4-pug-stylus
+$ cd myWebsite
+$ npm install | yarn install
+```
+
+If you don't wish to clone, you can [download the source code](https://github.com/template-gulp-4-pug-stylus/archive/master.zip)
+
+## Launch
+
+To start development
+```
+$ gulp pug
+$ gulp 
+```
+
+To build the project
+```
+$ yarn build-webpack | npm run build-webpack
+```
---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+"use strict";
+
+const gulp = require('gulp'),
+      stylus = require('gulp-stylus'),
+      pug = require('gulp-pug'),
+      inheritance = require('gulp-pug-inheritance'),
+      autoprefixer = require('gulp-autoprefixer'),
+      browserSync = require('browser-sync');
+
+gulp.task('styles', () => {
+    return gulp.src('./src/static/stylus/index.styl')
+        .pipe(stylus({
+            'include css': true
+        }))
+        .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], {cascade: true}))
+        .pipe(gulp.dest('./src/public/style/'))
+        .pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('serve', () => {
+    browserSync({
+        server: {
+			baseDir: './src'
+		},
+        notify: false
+    });
+});
+
+gulp.task('pug', () => {
+    return gulp.src('./src/static/pug/**/*.pug')
+        .pipe(inheritance({basedir: './src/static/pug/', skip: 'node_modules'}))
+        .pipe(pug({
+            pretty: true
+        }))
+        .pipe(gulp.dest('./src/'))
+        .on('end', browserSync.reload);
+});
+
+gulp.task('watch', () => {
+    gulp.watch('./src/static/stylus/**/*.styl', gulp.parallel('styles'));
+    gulp.watch('./src/static/pug/**/*.pug', gulp.parallel('pug'));
+});
+
+gulp.task('default', gulp.parallel('styles', 'serve', 'watch'));

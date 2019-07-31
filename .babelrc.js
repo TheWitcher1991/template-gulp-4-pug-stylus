7b5cbe5b65423e7a639edc58fae2cef4bc34c6b5
TheWitcher1991 <ashot.svazyan222@gmail.com>
2019-07-31 14:12:42 +0300

    start

---
+++ .babelrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    'presets': {
+        'env': {
+            'targets': {},
+            'useBuiltIns': false,
+            'loose': false,
+            'spec': false,
+            'uglify': false,
+            'debug': false,
+            'modules': 'commonjs',
+            'include': [],
+            'exclude': []
+        }
+    }
+}
